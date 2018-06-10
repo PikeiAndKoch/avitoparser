9030b8f74874e22cf954a318d80568bc078bd650
Oleg Kochiev <developer-artisan@mail.ru>
2018-06-10 10:13:04 +0300

    Подход к работе с БД изменен с функционального на ООП

--- ConnectionToDB.php
+++ ConnectionToDB.php
@@ -1,37 +1,47 @@
 <?php
 
-$host = '127.0.0.1';
-$db = 'AvitoAds';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, $user, $pass, $opt);
-
-function Write_On_DB($arr)
+class AvitoDB
 {
-    $md5 = '';
-    foreach ($arr as $key=>$value) {
-        global $pdo;
-        $md5 = md5($value[4]);
-        $stmt = $pdo->prepare('SELECT * FROM Ads WHERE hash = :md5');
-        $stmt->bindParam('md5', $md5);
-        $stmt->execute();
-        if ($stmt->fetchAll() == false) {
-            $value[] = $md5;
-            $stmt = $pdo->prepare('INSERT INTO ADS (ad_name, ad_price, ad_loc, ad_time, ad_src, hash) VALUES (?, ?, ?, ?, ?, ?)');
-            $stmt->execute($value);
-        }
+    private $_host;
+    private $_db;
+    private $_name;
+    private $_pass;
+    private $_charset;
+    const OPT = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    private $_pdo;
+    public function __construct($host, $db, $name, $pass, $charset){
+        $this->_host = $host;
+        $this->_db = $db;
+        $this->_name = $name;
+        $this->_pass = $pass;
+        $this->_charset = $charset;
+        $dsn = sprintf(
+            "mysql:host=%s;dbname=%s;charset=%s",
+            $this->_host,
+            $this->_db,
+            $this->_chatset
+        );
+        $this->_pdo = new PDO(
+            $dsn,
+            $this->_user,
+            $this->_pass,
+            OPT
+        );
     }
-}
+    
+    public function get_ads(){
 
-function Read_On_DB()
-{
+    }
+    public function set_ads(){
 
-}
+    }
+
+
+}   
+
+
+
